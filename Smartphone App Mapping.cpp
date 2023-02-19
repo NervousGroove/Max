12dd225a493175e7558a69da26125ad46a09c051
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-19 18:26:20 -0300

    Create Smartphone App Mapping.cpp

---
+++ Smartphone App Mapping.cpp
@@ -0,0 +1,12 @@
+//NECESSARY APP: Arduino Bluetooth Controller by MyValley
+//C++ code
+//Max By SwankyNoob
+//CC-NC-ND
+//Date of this Update: 02.19.2023
+
+//Map of Signals:
+
+//A = Walk FORWARD
+//a = Walk to the RIGHT side
+//B = Walk to the LEFT side
+//b = Walk to the BACKWARDS
