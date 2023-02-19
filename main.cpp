614dfd3f14fe3fd822acfcb040a604fbe7082ac8
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-19 17:55:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,22 @@
 //CC-NC-ND
 //Date of this Update: 02.19.2023
 
-int MaxInput13 = 13;
-int MaxInput12 = 12;
-int MaxInput11 = 11;
-int MaxInput10 = 10;
-int MaxInput9 = 9;
-int MaxInput8 = 8;
+int MaxInput1 = 13;
+int MaxInput2 = 12;
+int MaxInput3 = 11;
+int MaxInput4 = 10;
+int MaxInput5 = 9;
+int MaxInput6 = 8;
 
 //Max Tip: The setup() function is executed only once, usually used for Max project setups.
 void setup()
 {
-  pinMode(MaxInput13, OUTPUT);
-  pinMode(MaxInput12, OUTPUT);
-  pinMode(MaxInput11, OUTPUT);
-  pinMode(MaxInput10, OUTPUT);
-  pinMode(MaxInput9, OUTPUT);
-  pinMode(MaxInput8, OUTPUT);
+  pinMode(MaxInput1, OUTPUT);
+  pinMode(MaxInput2, OUTPUT);
+  pinMode(MaxInput3, OUTPUT);
+  pinMode(MaxInput4, OUTPUT);
+  pinMode(MaxInput5, OUTPUT);
+  pinMode(MaxInput6, OUTPUT);
 }
 
 //Max's tip: The loop() function, always executed (as long as the program is active), is the main part of the Max project, and of any other Arduino project.
