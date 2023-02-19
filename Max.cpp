8e26013432ad9e54011a0fc9a7cb80bb77ef5f3b
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-19 18:13:52 -0300

    Update Max.cpp

--- Max.cpp
+++ Max.cpp
@@ -19,10 +19,53 @@ void setup()
   pinMode(MaxInput4, OUTPUT);
   pinMode(MaxInput5, OUTPUT);
   pinMode(MaxInput6, OUTPUT);
+  Serial.begin(9600);
 }
 
 //Max's tip: The loop() function, always executed (as long as the program is active), is the main part of the Max project, and of any other Arduino project.
-void loop()
-{
 
+void loop() 
+{
+  if(Serial.available() > 0)
+  {
+      dado_recebido = Serial.read();
+      switch(dado_recebido)
+      {
+        case 'A':
+                  digitalWrite(LED3,HIGH);
+                  digitalWrite(LED8,LOW);
+                  Serial.println("Received Max Command:");
+                  Serial.println("Walk Forward");
+        break;
+
+        case 'a':
+                  digitalWrite(LED3,LOW);
+                  digitalWrite(LED8,HIGH);
+                  Serial.println("Received Max Command:");
+                  Serial.println("Walking on the Right Side");
+                  
+        break;
+
+        case 'B':
+                  digitalWrite(LED3,LOW);
+                  digitalWrite(LED8,HIGH);
+                  Serial.println("Received Max Command");
+                  Serial.println("Walk to the Left Side");
+                  
+        break;
+
+        case 'b':
+                  digitalWrite(LED3,LOW);
+                  digitalWrite(LED8,HIGH);
+                  Serial.println("Received Max Command:");
+                  Serial.println("Walking Backwards");
+                  
+        break;    
+        
+        default: 
+        Serial.println("Max Problem: Invalid Command");
+       }
+    
+  }
+         
 }
