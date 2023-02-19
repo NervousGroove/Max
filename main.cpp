b406106b46792d862baa8ea22ad61db2a2174768
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-19 17:33:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,3 +2,25 @@
 //Max By SwankyNoob
 //CC-NC-ND
 //Date of this Update: 02.19.2023
+
+int MaxInput13 = 13;
+int MaxInput12 = 12;
+int MaxInput11 = 11;
+int MaxInput10 = 10;
+int MaxInput9 = 9;
+int MaxInput8 = 8;
+
+void setup()
+{
+  pinMode(MaxInput13, OUTPUT);
+  pinMode(MaxInput12, OUTPUT);
+  pinMode(MaxInput11, OUTPUT);
+  pinMode(MaxInput10, OUTPUT);
+  pinMode(MaxInput9, OUTPUT);
+  pinMode(MaxInput8, OUTPUT);
+}
+
+void loop()
+{
+
+}
