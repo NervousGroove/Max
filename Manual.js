860d06f620a51967b0fd746d36303f4512810487
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-20 19:58:51 -0300

    Update Manual.js

--- Manual.js
+++ Manual.js
@@ -1,12 +1,14 @@
 /*
 The bottom of the Max (the bottom) is a Fortek prototype, which is why it is so strong and sturdy. The power source for the Max is batteries, and battery life may vary depending on the brand of the battery and the level of use of the Max. The top part of the Max has no screws because it is completely made in 3D, and cannot be disassembled.
-The Bluetooth connection of the Max works with an HC-05, and can be configured, as the name of the connection, and other aspects.
+The Bluetooth connection of the Max works with an HC-05, and can be configured, as the name of the connection, and other aspects. The camera we chose to use in the Max is an A9, which supports high-tech features such as Night Vision and high quality footage.
 
 Average Cost for Assembly (Based on 2023, in the Mercado Libre):
 
-- Arduino Uno: $14.51
-- Fortek Base Kit (Bottom): $13.74
-- HC-05: $5,80
+- Arduino Uno: $14.51 [https://shre.ink/arduinouno]
+- Fortek Base Kit (Bottom): $13.74 [https://shre.ink/fortekcar]
+- HC-05: $5,80 [https://shre.ink/h05bluetooth]
 
 Total: $34.05
+
+{+ Camera = $5,00 [https://shre.ink/minicamera]}
 */
