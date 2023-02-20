611c5afb1fd04c1aa8a1acb553596b9e140c4427
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-20 16:42:03 -0300

    Create Manual.js

---
+++ Manual.js
@@ -0,0 +1,12 @@
+/*
+The bottom of the Max (the bottom) is a Fortek prototype, which is why it is so strong and sturdy. The power source for the Max is batteries, and battery life may vary depending on the brand of the battery and the level of use of the Max. The top part of the Max has no screws because it is completely made in 3D, and cannot be disassembled.
+The Bluetooth connection of the Max works with an HC-05, and can be configured, as the name of the connection, and other aspects.
+
+Average Cost for Assembly (Based on 2023, in the Mercado Libre):
+
+- Arduino Uno: $14.51
+- Fortek Base Kit (Bottom): $13.74
+- HC-05: $5,80
+
+Total: $34.05
+*/
