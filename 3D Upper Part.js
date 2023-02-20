4b24e6e180339e438317cdbe514b15e34062c5c8
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-20 19:16:40 -0300

    Delete 3D Upper Part.js

--- 3D Upper Part.js
+++
@@ -1,4 +0,0 @@
-/*
-Edit and/or view the project from the top of Max, in Tinkercad:
-https://www.tinkercad.com/things/8QhHZAyGg4x-frantic-krunk/edit?sharecode=NW-O6-pnoWvpdY1fpIIQMibtJSQg06OTkMdKScK_IkQ
-*/
