af96bc2a4c79d8f7f040684cdc9f6906f2be78e7
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-02-21 16:40:18 -0300

    Update Max.cpp

--- Max.cpp
+++ Max.cpp
@@ -5,6 +5,10 @@
 
 char dado_recebido = 0;
 int MaxVersionInfo = 100;
+int FrontEngine = 13;
+int FrontEngine2 = 12;
+int RearEngine = 11;
+int RearEngine2 = 10;
 
 //Max Tip: The setup() function is executed only once, usually used for Max project setups.
 void setup()
@@ -31,35 +35,55 @@ void loop()
       {
         //If Max receives the A (Capital) signal from the smartphone, he will walk forward.
         case 'A':
-                  digitalWrite(LED3,HIGH);
-                  digitalWrite(LED8,LOW);
+                  digitalWrite(FrontEngine, HIGH);
+                  digitalWrite(FrontEngine2, HIGH);
+                  digitalWrite(RearEngine, HIGH);
+                  digitalWrite(RearEngine2, HIGH);
                   Serial.println("Received Max Command:");
                   Serial.println("Walk Forward");
         break;
         //If Max receives the a (Minuscule) signal from the smartphone, he will walk to the RIGHT side.
         case 'a':
-                  digitalWrite(LED3,LOW);
-                  digitalWrite(LED8,HIGH);
+                 
+          
+                  digitalWrite(RearEngine, HIGH);
+                  digitalWrite(RearEngine2, HIGH);
+                  digitalWrite(FrontEngine2, HIGH);
                   Serial.println("Received Max Command:");
                   Serial.println("Walking on the Right Side");
                   
         break;
         //If Max receives the B (Capital) signal from the smartphone, he will walk to the LEFT side.
         case 'B':
-                  digitalWrite(LED3,LOW);
-                  digitalWrite(LED8,HIGH);
+                  digitalWrite(FrontEngine, HIGH);
+                  digitalWrite(RearEngine, HIGH);
+                  digitalWrite(RearEngine2, HIGH);
+                  digitalWrite(FrontEngine2, LOW);
                   Serial.println("Received Max Command");
                   Serial.println("Walk to the Left Side");
                   
         break;
         //If Max receives the b (Minuscule) signal from the smartphone, he will walk to the backwards.   
         case 'b':
-                  digitalWrite(LED3,LOW);
-                  digitalWrite(LED8,HIGH);
+                  digitalWrite(FrontEngine, LOW);
+                  digitalWrite(RearEngine, HIGH);
+                  digitalWrite(RearEngine2, HIGH);
+                  digitalWrite(FrontEngine2, LOW);
                   Serial.println("Received Max Command:");
                   Serial.println("Walking Backwards");
                   
-        break;    
+        break;
+          
+        //If Max receives the b (Minuscule) signal from the smartphone, he brakes.   
+        case 'C':
+                  digitalWrite(FrontEngine, LOW);
+                  digitalWrite(RearEngine, LOW);
+                  digitalWrite(RearEngine2, LOW);
+                  digitalWrite(FrontEngine2, LOW);
+                  Serial.println("Received Max Command:");
+                  Serial.println("Walking Backwards");
+                  
+        break;
         
         default: 
         Serial.println("Max Problem: Invalid Command");
